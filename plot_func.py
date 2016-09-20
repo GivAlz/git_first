873efbcc81464fca3e89f98e26968f49e55bffe3
denocris <denobili16@gmail.com>
2016-09-20 16:02:40 +0200

    creazione file plot_func.py

---
+++ plot_func.py
@@ -0,0 +1,6 @@
+# A student's packagies
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt

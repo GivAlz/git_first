628be6f3b63dce8669cc3a7c958e1a21325047b0
GivAlz <giovannialzetta@hotmail.it>
2016-09-20 16:11:40 +0200

    restart

--- plot_func.py
+++ plot_func.py
@@ -1,6 +1,1 @@
-# A student's packagies
 
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import matplotlib.pyplot as plt

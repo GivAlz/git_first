3f3618ac4dd193ecfedc8dfa2d93ff88a19b6260
Giovanni <giovannialzetta@hotmail.it>
2016-09-20 16:21:05 +0200

    function evaluation

--- draw_a_graph.py
+++ draw_a_graph.py
@@ -8,6 +8,13 @@
 
 def eval_func():
     
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X**2 + Y**2)
+    Z = np.sin(R)
+
+    
     return X,Y,Z
 
 # A student's function
@@ -19,3 +26,5 @@ def plot_func(X,Y,Z):
 # B student writes the main
 
 if __name__=="__main__":
+    
+    X,Y,Z = eval_func()
